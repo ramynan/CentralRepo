41c3cabac09e454e664e3d5023d5e91f6836f335
Nanjundappa <ramya.nanjundappa@accenture.com>
2018-12-02 15:20:10 +0530

    adding files

--- firstclass.java
+++ firstclass.java
@@ -1,7 +1,12 @@
 class firstclass{
 	public static void main(String []args)
 	{
+			int i;
 			System.out.println("hey i am the user");
+			for (i=0;i<2;i++)
+			{
+				System.out.println("just printing" +i);
+			}
 			
 	}
 }
